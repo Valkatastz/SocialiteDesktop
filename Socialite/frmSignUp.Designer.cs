2f2e1a23ba25e8d735a0b19890724e8315d50559
valkatastz <valentins613@gmail.com>
2016-12-01 23:42:27 +0000

    Register created

--- Socialite/frmSignUp.Designer.cs
+++ Socialite/frmSignUp.Designer.cs
@@ -28,20 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblReg = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblEmailCheck = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblReg
+            // 
+            this.lblReg.AutoSize = true;
+            this.lblReg.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReg.Location = new System.Drawing.Point(458, 75);
+            this.lblReg.Name = "lblReg";
+            this.lblReg.Size = new System.Drawing.Size(233, 26);
+            this.lblReg.TabIndex = 0;
+            this.lblReg.Text = "Account Registration";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(472, 183);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(472, 209);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(472, 235);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(472, 261);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.TabIndex = 4;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(472, 287);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(561, 180);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 6;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(561, 206);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '@';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(561, 232);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 8;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(561, 258);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 9;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(561, 284);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 10;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(498, 352);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(128, 23);
+            this.btnCreate.TabIndex = 11;
+            this.btnCreate.Text = "Create Account";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.AutoSize = true;
+            this.lblCheck.Location = new System.Drawing.Point(691, 183);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(0, 13);
+            this.lblCheck.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 493);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(525, 18);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "© Socialite 2016 - DMU Team Development Under Graduate Final Year Project";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(522, 381);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 14;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblEmailCheck
+            // 
+            this.lblEmailCheck.AutoSize = true;
+            this.lblEmailCheck.Location = new System.Drawing.Point(691, 239);
+            this.lblEmailCheck.Name = "lblEmailCheck";
+            this.lblEmailCheck.Size = new System.Drawing.Size(0, 13);
+            this.lblEmailCheck.TabIndex = 15;
             // 
             // frmSignUp
             // 
+            this.AccessibleDescription = "";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(758, 511);
+            this.Controls.Add(this.lblEmailCheck);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblReg);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "frmSignUp";
             this.Text = "Sign Up";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblReg;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lblCheck;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblEmailCheck;
     }
 }
--- Socialite/frmSignUp.cs
+++ Socialite/frmSignUp.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace Socialite
 {
@@ -15,6 +17,70 @@ namespace Socialite
         public frmSignUp()
         {
             InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            frmLogin signed = new frmLogin();
+            SqlConnection account = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\TeamDev\Socialite\Socialite\Socialite\SocialiteDB.mdf;Integrated Security=True;Connect Timeout=30");
+            account.Open();
+
+            if (lblCheck.ForeColor == System.Drawing.Color.Green && lblEmailCheck.ForeColor == System.Drawing.Color.Green)
+            {
+                SqlCommand add = new SqlCommand("Insert into Accounts(Username, Password, First_Name, Last_Name, Email) Values('" + txtUsername.Text + "', '" + txtPassword.Text + "' ,  '" + txtEmail.Text + "' , '" + txtFirstName.Text + "' , '" + txtLastName.Text + "')", account);
+                add.ExecuteNonQuery();
+                account.Close();
+                this.Hide();
+                signed.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please, choose different Username or Email");
+            }
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection account = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\TeamDev\Socialite\Socialite\Socialite\SocialiteDB.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter acadp = new SqlDataAdapter("Select Count (*) From Accounts where Username='" + txtUsername.Text + "'", account);
+            DataTable adt = new DataTable();
+            acadp.Fill(adt);
+            if (int.Parse(adt.Rows[0][0].ToString()) == 0)
+            {
+                lblCheck.Text = txtUsername.Text + " is Available";
+                this.lblCheck.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblCheck.Text = txtUsername.Text + " is not available";
+                this.lblCheck.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection account = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\TeamDev\Socialite\Socialite\Socialite\SocialiteDB.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter acadp = new SqlDataAdapter("Select Count (*) From Accounts where Email='" + txtEmail.Text + "'", account);
+            DataTable adt = new DataTable();
+            acadp.Fill(adt);
+            if (int.Parse(adt.Rows[0][0].ToString()) == 0)
+            {
+                lblEmailCheck.Text = txtEmail.Text + " is Available";
+                this.lblEmailCheck.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblEmailCheck.Text = txtEmail.Text + " is not available";
+                this.lblEmailCheck.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            frmLogin login = new frmLogin();
+            login.Show();
+            this.Close();
         }
     }
 }
